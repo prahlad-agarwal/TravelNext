f94da47acf5329faf5fbe00a2e8610a7f3abd39b
prahladagarwal <prahladagarwal575@gmail.com>
2026-02-06 15:51:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://travelnext-frontend-folder-rvvm.onrender.com",
     credentials: true,
   })
 );
