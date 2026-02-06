56ff1422734f68368bcde6f6fc485e4821806e59
prahladagarwal <prahladagarwal575@gmail.com>
2026-02-06 15:27:23 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 export const authDataContext = createContext();
 
 export const AuthContext = ({ children }) => {
-  const serverUrl = "https://travelnext-backend-y1fz.onrender.com";
+  const serverUrl = "http://localhost:8080";
 
   const [loading, setLoading] = useState(false);
 
